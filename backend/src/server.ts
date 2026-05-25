741fecdcc77df528b76e00d1e1c149c22486ac94
Andy Mena <andy.haniel.mena@gmail.com>
2026-05-25 14:32:40 -0600

    endpoint to check if the server is running

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,5 @@
 import express, {Express} from "express"
 import cors from "cors";
-import colors from "colors";
 import { connectDB } from "./config/database";
 import dotenv from "dotenv";
 import morgan from "morgan";
@@ -17,7 +16,16 @@ server.use(express.json());
 server.use(cors(corsConfig));
 server.use(morgan("dev"));
 
+//Test endpoint to check if the server is running
+server.get("/api/v1/health", (req, res) => {
+    res.status(200).json({ 
+        status: 'ok', 
+        message: 'El servidor está despierto',
+        timestamp: new Date()
+    });
+})
 
+//Routes
 server.use("/api/v1/user", userRoute); //route for users
 server.use("/api/v1/site", sitesRoute); //route for sites
 server.use("/api/v1/work-report", workReportRoute); //route for work reports
